f3616c9db7a149d2832ca492821bd5ed0fa832dc
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-02-11 18:55:50 +0200

    feat: update ui elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
+    colors:{
+      'cream': '#F0F0C9',
+      'outerspace': '#3E4E50',
+      'teal-900':'#134e4a',
+       orange:colors.orange,
+       white: colors.white,
+    },
     extend: {},
   },
   plugins: [],
